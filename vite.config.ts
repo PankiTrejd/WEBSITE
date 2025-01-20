81ef073dbda102d207254c838bff3e1de7dadbb2
PankiTrejd <cncdecorates@gmail.com>
2025-01-20 17:41:44 +0200

    all

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  plugins: [react()], // Remove mkcert
+  server: {
+    https: false, // Disable HTTPS
+    port: 5174, // Use the port you need
+    strictPort: true, // Ensure the server fails if the port is not available
+    open: true, // Automatically open the browser
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
+  base: './',
 });
